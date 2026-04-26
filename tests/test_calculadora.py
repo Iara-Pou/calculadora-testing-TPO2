44d54393dea56ed78c7eb5505166f1e456708008
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2026-04-26 15:35:20 -0300

    Agrega control de mensajes de error

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -17,18 +17,21 @@ def test_division_correcta():
 
 ## Casos error
 def test_division_por_cero():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as excinfo:
         calcular(10, 0, "division")
+    assert str(excinfo.value) == "No se puede dividir por cero"
 
 def test_operacion_invalida():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError) as excinfo:
         calcular(2, 2, "raíz cuadrada")
+    assert str(excinfo.value) == "Operación no válida"
 
 
 ## Casos border
 def test_input_decimal_invalido():
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeError) as excinfo:
         calcular(2.3, 3, "suma")
+    assert str(excinfo.value) == "Los operandos deben ser números enteros"
 
 def test_division_decimal():
     assert calcular(5, 2, "division") == 2.5
